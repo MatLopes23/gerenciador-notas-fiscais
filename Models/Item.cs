360f19936d13e79809e6a72861139dd45deb330e
Mateus Lopes <mateusfellipe2302@gmail.com>
2023-09-20 10:53:34 -0300

    🎉 Commit inicial

---
+++ Models/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gerenciador_de_Notas_Fiscais.Models
+{
+
+
+    [Table("Item")]
+    public class Item
+    {
+        [Key]
+        [Column("IdItem")]
+        public int IdItem { get; set; }
+
+        [Column("Produto")]
+        public string Produto { get; set; }
+
+        [Column("Valor")]
+        public decimal Valor { get; set; }
+
+        [ForeignKey("NotaFiscal")]
+        [Column("IdNota")]
+        public int IdNota { get; set; }
+
+        public required NotaFiscal NotaFiscal { get; set; }
+    }
+
+}
---
+++ Models/NotaFiscal.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gerenciador_de_Notas_Fiscais.Models
+{
+    [Table("NotaFiscal")]
+    public class NotaFiscal
+    {
+        [Key]
+        [Column("IdNota")]
+        public int IdNota { get; set; }
+
+        [Column("Emissor")]
+        public string? Emissor { get; set; }
+
+        [Column("Data")]
+        [DataType(DataType.Date)]
+        public DateTime Data { get; set; }
+
+        public required ICollection<Item> Itens { get; set; }
+    }
+
+}
